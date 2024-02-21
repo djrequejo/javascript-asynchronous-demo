091b722b3e751f9cddaca8e8fb90bc3bbf97ac4a
Diego Requejo Torero <djrequejo@gmail.com>
2024-02-21 01:04:20 -0500

    Refactoring code with async and await. Add generator examples.

---
+++ src/4-generators/0-intro.js
@@ -0,0 +1,27 @@
+// Example 1
+function* gen() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const g = gen();
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+
+// Example 2
+function* iterate(array) {
+  for(let value of array) {
+    yield value;
+  }
+}
+
+const it = iterate(['Diego', 'DJ', 'Nono','Nona']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
